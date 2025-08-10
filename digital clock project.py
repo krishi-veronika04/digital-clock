4beaa51323550b42e72ab1747d7e2430e1e563e4
krishi-veronika04 <krishiveronika5@gmail.com>
2025-08-10 17:32:20 +0530

    Add files via upload

---
+++ digital clock project.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import time
+def digital():
+    d = time.strftime("%H:%M:%S")
+    clock.config(text=d)
+    clock.after(200,digital)
+root=Tk()
+root.title("digital clock - vero robots")
+clock=Label(root,font=("times",100,"bold"),bg="blue")
+clock.grid(row=2,column=1)
+digital()
+root.mainloop()
